41aff08030ba9fe59c0458f6c63a22d1b616d5a1
avanindrathakur <72639956+avanindrathakur@users.noreply.github.com>
2021-03-26 17:55:47 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,44 @@
+
+library(shiny)
+
+shinyUI(
+  navbarPage("Shiny App",
+             tabPanel("Exploratory data Analysis",
+                      fluidPage(
+                        titlePanel("Determinants of Miles per Gallen(MPG)"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            selectInput("variable", "Variable:",
+                                        c("Number of cylinders" = "cyl",
+                                          "Displacement (cu.in.)" = "disp",
+                                          "Gross horsepower" = "hp",
+                                          "Rear axle ratio" = "drat",
+                                          "Weight (lb/1000)" = "wt",
+                                          "1/4 mile time" = "qsec",
+                                          "V/S" = "vs",
+                                          "Transmission" = "am",
+                                          "Number of forward gears" = "gear",
+                                          "Number of carburetors" = "carb"
+                                        )),
+                            
+                            checkboxInput("outliers", "Show BoxPlot's outliers", FALSE)
+                          ),
+                          
+                          mainPanel(
+                            h3(textOutput("caption")),
+                            
+                            tabsetPanel(type = "tabs", 
+                                        tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
+                                        tabPanel("Linear model", 
+                                                 plotOutput("mpgPlot"),
+                                                 verbatimTextOutput("fit")
+                                        )
+                            )
+                          )
+                        )
+                      )
+             )
+  )
+)
+
+                      
